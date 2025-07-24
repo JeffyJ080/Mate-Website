86f70bcfc5b14582c50e2baa59a6547d0e951aee
Jeffy Jefferson <jeffyj080@gmail.com>
2025-07-24 14:46:13 +0200

    Add project files.

---
+++ Mate Website/Scripting.js
@@ -0,0 +1,8 @@
+﻿function scrollToContact() {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+}
+
+function showSuccessMessage() {
+    document.getElementById('success-message').style.display = 'block';
+    return true;
+}
